20f10d815f38f536f663ef0bf9cc03824b621567
nSedrickm <nsedrick101@gmail.com>
2021-01-17 09:48:45 +0100

    add carbon tutorial skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.scss';
+import { Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <TutorialHeader />
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+          </Switch>
+        </Content>
+      </>
+    );
+  }
 }
 
 export default App;
