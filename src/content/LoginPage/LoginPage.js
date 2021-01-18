67d3cdac38b090c3edd11304e260bcad326d0198
nSedrickm <nsedrick101@gmail.com>
2021-01-18 18:38:41 +0100

    can now log form data

--- src/content/LoginPage/LoginPage.js
+++ src/content/LoginPage/LoginPage.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form, TextInput, Button } from 'carbon-components-react';
 import { ArrowRight24 } from '@carbon/icons-react';
 
 
 
 const LoginPage = () => {
+
+  const username = useRef(null);
+  const password = useRef(null);
+  const loginUrl = useRef(null);
+
+  const formProps = {
+    onSubmit: (e) => {
+      e.preventDefault();
+      const loginData = {
+        username: username.current.value,
+        password: password.current.value,
+        loginUrl: loginUrl.current.value,
+      }
+
+      console.log(loginData);
+      //Todo : actually do something with this info
+    },
+  };
+
   return (
     <div className="bx--grid login-page__container">
       <div className="bx--row">
@@ -12,24 +31,27 @@ const LoginPage = () => {
           <h1 className="login-page__title"><strong>Z </strong> | Connect</h1>
           <p>Connect to z/Os on mobile</p>
 
-          <Form>
+          <Form {...formProps}>
             <TextInput
-              id="test2"
               invalidText="Invalid error message."
               labelText="User Name"
               placeholder="enter username "
+              id="username"
+              ref={username}
             />
             <TextInput
-              id="test2"
               invalidText="Invalid error message."
               labelText="Password"
               placeholder="your password here"
+              id="password"
+              ref={password}
             />
             <TextInput
-              id="test2"
               invalidText="Invalid error message."
               labelText="Web Url"
               placeholder="https://<ip address>:<port>"
+              id="loginUrl"
+              ref={loginUrl}
             />
             <Button
               className="submit-button"
